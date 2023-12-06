4e0716f929d817d61f6aeab25e5783d7eba6385c
Radu Cebotari <radu.cebotari@green-share.it>
2023-12-06 21:23:20 +0100

    wip

--- calctende_app/calctende_app/settings.py
+++ calctende_app/calctende_app/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -140,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- calctende_app/core/views.py
+++ calctende_app/core/views.py
@@ -1,3 +1,42 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
+
+
+from django import forms
+
+
+class WaveForm(forms.Form):
+    your_name = forms.CharField(label="Your name", max_length=100)
+    interval = forms.DurationField(label="sdgfdg")
 # Create your views here.
+class CalcWaves(View):
+
+
+    def get(self, request, *args, **kwargs):
+        form = WaveForm()
+        return render(request, 'base.html', {'form':form})
+    
+    def post(self, request, *args, **kwargs):
+        form = WaveForm()
+        if form.is_valid():
+            form.save()
+            return redirect('calcwave')
+        return render(request, 'base.html', {'form':form})
+
+
+class AddPage(View):
+    def get(self, request):
+        form = AddPostForm()
+        return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи', 'form': form})
+ 
+    def post(self, request):
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+ 
+        return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи', 'form': form})
+
+
