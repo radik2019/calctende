f0b24dfe149c70bd1a768639537cdb314acd5f6c
Radu Cebotari <radu.cebotari@green-share.it>
2023-12-12 08:59:25 +0100

    wip

--- core/models.py
+++ core/models.py
@@ -6,13 +6,34 @@ class TimeStamped(models.Model):
     created = models.DateTimeField(auto_created=True)
     modified = models.DateTimeField(auto_now=True)
 
+
 class ServiceType(TimeStamped):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    extra_data = models.JSONField(default={})
+
+
+class Discount(models.Model):
+    """
+    Discount applicable to the price of a service or product
+    """
+    name = models.CharField(max_length=255, default="Sconto Generico")
+    discount_percentage = models.FloatField(help_text="sconto")
+
+
+class Cost(models.Model):
     unit_of_measure = models.CharField(choices=('metro, centimetro, chilo, pezzi'), default='metro',
         max_length=255, help_text="unita` di misura per il prezzo (metri, pezzi, centimetri)")
     price = models.FloatField(help_text="prezzo all'unita` di misura")
-    # user = models.ForeignKey(User, "")#User()
+    discount = models.ForeignKey(Discount, on_delete=models.PROTECT)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.ForeignKey(Cost, on_delete=models.PROTECT)
+    discount = models.ForeignKey(Discount, on_delete=models.PROTECT)
+
 
 class Customer(AbstractUser):
     """"""
