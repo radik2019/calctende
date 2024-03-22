c7175b8885fe4279d82105cc7dae87c83e70f132
Radu Cebotari <radu.cebotari@green-share.it>
2024-03-22 23:35:53 +0100

    fix: docker compose

--- calctende_app/settings.py
+++ calctende_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', "V23IdVTTT55iKxLx5fEWh7uO8OdT9jN194VfDFLY5fJnbOGBd6hpeqYQcICZFiEN")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", True)
